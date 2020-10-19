98496cf0181cc4f62ea3ccd2c79d783517c471e7
Maxime Gosselin <maxime.gosselin@cssspnql.com>
2020-10-19 11:45:36 -0400

    feat: add ProjectionStoreTransactionCommandBusMiddleware

---
+++ src/ProjectionStoreTransactionCommandBusMiddleware/ProjectionStoreTransactionCommandBusMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BusFactor\ProjectionStoreTransactionCommandBusMiddleware;
+
+use BusFactor\CommandBus\CommandDispatcherInterface;
+use BusFactor\CommandBus\MiddlewareInterface;
+use BusFactor\ProjectionStore\ProjectionStoreInterface;
+use Throwable;
+
+class ProjectionStoreTransactionCommandBusMiddleware implements MiddlewareInterface
+{
+    private ProjectionStoreInterface $projections;
+
+    private int $nestedLevels;
+
+    public function __construct(ProjectionStoreInterface $projections)
+    {
+        $this->projections = $projections;
+        $this->nestedLevels = 0;
+    }
+
+    public function dispatch(object $command, CommandDispatcherInterface $next): void
+    {
+        try {
+            $this->nestedLevels++;
+            $next->dispatch($command);
+            $this->nestedLevels--;
+            if ($this->nestedLevels === 0) {
+                $this->projections->commit();
+            }
+        } catch (Throwable $t) {
+            $this->projections->rollback();
+            throw $t;
+        }
+    }
+}
