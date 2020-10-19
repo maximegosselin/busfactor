5c04098de5e014e2263d12d4f866cdb8cd02e0ea
Maxime Gosselin <maxime.gosselin@cssspnql.com>
2020-10-19 10:39:05 -0400

    refactor: remove Problem

--- src/EventStore/EventStoreException.php
+++ src/EventStore/EventStoreException.php
@@ -4,26 +4,8 @@ declare(strict_types=1);
 
 namespace BusFactor\EventStore;
 
-use BusFactor\Problem\ProblemDetailsAwareInterface;
-use BusFactor\Problem\ProblemDetailsAwareTrait;
 use Exception;
 
-class EventStoreException extends Exception implements ProblemDetailsAwareInterface
+class EventStoreException extends Exception
 {
-    use ProblemDetailsAwareTrait;
-
-    public function getStatus(): int
-    {
-        return 500;
-    }
-
-    public function getType(): string
-    {
-        return 'event-store-exception';
-    }
-
-    public function getTitle(): string
-    {
-        return 'event-store-exception';
-    }
 }
